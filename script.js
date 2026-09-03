dd2345a7694f820bf00f168bf851966c78efb5f4
Abhishek Verma <abhishekverma2289@gmail.com>
2026-09-03 19:02:11 +0530

    Update API URL

--- script.js
+++ script.js
@@ -5,7 +5,7 @@
     // FastAPI URL
     // =========================================================
 
-    const API_BASE = "http://127.0.0.1:8000";
+    const API_BASE = "https://manshik-santulan-score-ozlh.onrender.com";
 
 
     // =========================================================
